ca3db17278408ec8e0d59e7a2dd744bd8dd631f7
Conor Fleming <conorfleming@outlook.com>
2020-12-31 13:28:23 -0600

    content path to be taken from containing folder

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const { createFilePath } = require("gatsby-source-filesystem")
 
 const TAGS_FOR_WHICH_TO_CREATE_LIST_VIEWS = ["articles", "stories"]
 const NUMBER_OF_POSTS_PER_PAGE_IN_LIST_VIEW = 12
@@ -44,8 +45,98 @@ async function paginate(tagName, { graphql, actions }) {
   })
 }
 
-exports.createPages = ctx => {
-  return Promise.all(
-    TAGS_FOR_WHICH_TO_CREATE_LIST_VIEWS.map(e => paginate(e, ctx))
+// Create pages for each item in the content directory
+async function createContentPages({ graphql, actions }) {
+  const template = path.resolve(__dirname, "src/templates/Content.js")
+
+  const { errors, data } = await graphql(
+    `
+      {
+        allMarkdownRemark() {
+          nodes {
+            id
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    `
   )
+
+  if (errors) {
+    throw new Error("There was an error creating content pages.")
+  }
+
+  const contentEdges = data.allMarkdownRemark.edges
+
+  contentEdges.forEach(e => {
+    const path = e.node.fields.slug
+    const id = e.node.id
+
+    actions.createPage({
+      path,
+      component: template,
+      context: {
+        id,
+      },
+    })
+  })
+}
+
+exports.createPages = ctx => {
+  return Promise.all([
+    ...TAGS_FOR_WHICH_TO_CREATE_LIST_VIEWS.map(e => paginate(e, ctx)),
+    createContentPages(ctx),
+  ])
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  if (node.internal.type === "MarkdownRemark") {
+    const value = createFilePath({ node, getNode })
+
+    actions.createNodeField({
+      name: "slug",
+      node,
+      value,
+    })
+  }
+}
+
+// Taken directly from starter-blog.
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions
+
+  // Explicitly define the siteMetadata {} object
+  // This way those will always be defined even if removed from gatsby-config.js
+
+  // Also explicitly define the Markdown frontmatter
+  // This way the "MarkdownRemark" queries will return `null` even when no
+  // blog posts are stored inside "content/blog" instead of returning an error
+  createTypes(`
+    type SiteSiteMetadata {
+      author: Author
+      siteUrl: String
+      social: Social
+    }
+    type Author {
+      name: String
+      summary: String
+    }
+    type Social {
+      twitter: String
+    }
+    type MarkdownRemark implements Node {
+      frontmatter: Frontmatter
+      fields: Fields
+    }
+    type Frontmatter {
+      title: String
+      description: String
+      date: Date @dateformat
+    }
+    type Fields {
+      slug: String
+    }
+  `)
 }
